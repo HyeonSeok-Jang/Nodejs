b7dfd1a9676c6260ea1ba6764cb3a27471794549
HyeonSeok-Jang <dkdidudtn@gmail.com>
2021-03-15 15:50:32 +0900

    Nodejs training

---
+++ Test.js
@@ -0,0 +1,18 @@
+window.onload = function(){
+    
+    
+    
+}
+console.log(sum1(2, 3));
+// console.log(sum2(2, 3));
+function sum1(a,b){
+    return a+b;
+}
+let sum2 = function(a,b){
+    return a+b;
+}
+let sum3 = (a,b)=>{
+    return a+b;
+}
+console.log(sum1(2, 3));
+console.log(sum2(2, 3));
